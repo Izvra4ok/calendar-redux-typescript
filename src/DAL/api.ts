92d8011d64f2b04b39871bdcc9129e49f894ca02
Izvra4ok <barzakouski.sergey@gmail.com>
2023-01-08 23:40:48 +0300

    some fix

--- src/DAL/api.ts
+++ src/DAL/api.ts
@@ -3,7 +3,7 @@ import {UserTypes} from "../Types/UserTypes";
 
 export default class UserService {
     static async getUser(): Promise<AxiosResponse<UserTypes[]>> {
-        return axios.get<UserTypes[]>("../user.json")
+        return axios.get<UserTypes[]>("./user.json")
     }
 
 }
